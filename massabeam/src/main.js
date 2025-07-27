c6b6390a3b15526efd28222bd5ecd61e43ff44a5
uzochukwuv <vic.ezealor@gmail.com>
2025-07-27 15:58:02 +0100

    build succesful2

--- massabeam/src/main.js
+++ massabeam/src/main.js
@@ -1,9 +1,6 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { MassaSwapSDK, MassaSwapUtils } from './massabeam.js';
-import { bytesToStr } from '@massalabs/massa-web3';
+
 
 
 
